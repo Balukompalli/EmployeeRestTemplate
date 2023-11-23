8ea6de503341a01a5ec2c87759a0b0bf312ad99c
Prasad <balumail2010@gmail.com>
2023-11-23 18:12:17 +0530

    rest template crud initial commit

--- src/main/java/com/employee/resttemplate/config/RestTemplateConfig.java
+++ src/main/java/com/employee/resttemplate/config/RestTemplateConfig.java
@@ -4,10 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
